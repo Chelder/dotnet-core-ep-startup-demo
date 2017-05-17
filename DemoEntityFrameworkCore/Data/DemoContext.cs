3b9de14366f67babb71b02e82f7c7f0f061bf573
Chelder Lins <Chelder Lins>
2017-05-16 22:58:15 -0300

    Aula 6 - Apresentando Entity Framework Core. Faltando comando add-migration

---
+++ DemoEntityFrameworkCore/Data/DemoContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StartupDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoEntityFrameworkCore.Data
+{
+    public class DemoContext : DbContext
+    {
+        public DemoContext(DbContextOptions<DemoContext> options)
+            :base(options)
+        {
+            
+        }
+
+        public DbSet<Evento> Eventos { get; set; }
+    }
+}
